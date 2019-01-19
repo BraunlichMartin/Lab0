add569a10438b2c2562b21f78b7ba5ff05cd9618
Christy Ganger <christyganger@gmail.com>
2019-01-18 16:10:30 -0800

    added stuff

---
+++ memory project/Memory/boardForm.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// there is a timer used in this program -MWB 1/16/2019
+using System.Timers;
+
+namespace Memory
+{
+    public partial class boardForm : Form
+    {
+        public boardForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Instance Variables
+        const int NOT_PICKED_YET = -1;
+
+        int firstCardNumber = NOT_PICKED_YET;
+        int secondCardNumber = NOT_PICKED_YET;
+        int matches = 0;
+        #endregion
+
+        #region Methods
+
+        // This method finds a picture box on the board based on it's number (1 - 20)
+        // It takes an integer as it's parameter and returns the picture box controls
+        // that's name contains that number
+        private PictureBox GetCard(int i)
+        {
+            PictureBox card = (PictureBox)this.Controls["card" + i];
+            return card;
+        }
+
+        // This method gets the filename for the image displayed in a picture box given it's number
+        // It takes an integer as it's parameter and returns a string containing the 
+        // filename for the image in the corresponding picture box
+        private string GetCardFilename(int i)
+        {
+            return GetCard(i).Tag.ToString();
+        }
+
+        // This method changes the filename for a given picture box
+        // It takes an integer and a string that represents a filename as it's parameters
+        // It doesn't return a value but stores the filename for the image to be displayed
+        // in the picture box.  It doesn't actually display the new image
+        private void SetCardFilename(int i, string filename)
+        {
+            GetCard(i).Tag = filename;
+        }
+
+        // These 2 methods get the value (and suit) of the card in a given picturebox
+        // Both methods take an integer as the parameter and return a string
+        private string GetCardValue(int index)
+        {
+            return GetCardFilename(index).Substring(4, 1);
+        }
+
+        private string GetCardSuit(int index)
+        {
+            return GetCardFilename(index).Substring(5, 1);
+        }
+
+        // TODO:  students should write this one
+        //added if else statement, before it just returned "true"-MWB_2019-1-14
+        private bool IsMatch(int index1, int index2)
+        {
+            if (GetCardValue(index1) == GetCardValue(index2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // This method fills each picture box with a filename
+        
+        
+        private void FillCardFilenames()
+        {
+            string[] values = { "a", "2", "j", "q", "k" };
+            string[] suits = { "c", "d", "h", "s" };
+            int i = 1;
+
+            for (int suit = 0; suit <= 3; suit++)
+            {
+                for (int value = 0; value <= 4; value++)
+                {
+                    
+                    SetCardFilename(i, "card" + values[value] + suits[suit] + ".jpg");
+                    i++;
+                }
+            }
+        }
+
+        // TODO:  students should write this one
+        //created a random number generator called rnd and applied it to the integers 1 through 4 to represent the card values, then created another instance inorder to represent suits 1 through 3
+        //-MWB_2019-1-14
+        
+        private void ShuffleCards()
+        {
+            /*
+            
+
+            for each card the method GetCardFilename will get the cards name and store it in the variable 'card1'
+            then a random number from one to twenty (inclusive) will be generated and assigned to 'value'
+            the method GetCardFilename will then be applied to this 'value' and assigned to the string 'card2'
+            then SetCardFilename is used to assign 'card2' to 'i'
+            then SetCardFilename is used to assign 'card1' to 'value'
+            overall this randomly assigns each card a new place in the ordering-MWB 1/18/2019
+            */
+            Random rnd = new Random();
+            for (int i = 1; i <= 20; i++)
+            {
+                string card1 = GetCardFilename(i);
+                int value = rnd.Next(1, 21);
+                string card2 = GetCardFilename(value);
+                SetCardFilename(i, card2);
+                SetCardFilename(value, card1);
+            }
+        }
+
+        // This method loads (shows) an image in a picture box.  Assumes that filenames
+        // have been filled in an earlier call to FillCardFilenames
+        private void LoadCard(int i)
+        {
+            PictureBox card = GetCard(i);
+            card.Image = Image.FromFile(System.Environment.CurrentDirectory + "\\Cards\\" + GetCardFilename(i));
+        }
+
+        // This method loads the image for the back of a card in a picture box
+        private void LoadCardBack(int i)
+        {
+            PictureBox card = GetCard(i);
+            card.Image = Image.FromFile(System.Environment.CurrentDirectory + "\\Cards\\black_back.jpg");
+        }
+
+        // TODO:  students should write all of these
+        // shows (loads) the backs of all of the cards
+        //-MWB 1-16-2019
+        private void LoadAllCardBacks()
+        {
+            for(int i=1; i <= 20; i++)
+            {
+                LoadCardBack(i);
+            }
+        }
+
+        //temporary function to make sure that all the cards are randomized and loaded in the event handler "boardformload "-MWB 1/16/2019
+        private void LoadAllCardFronts()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                LoadCard(i);
+            }
+        }
+
+        // Hides a picture box
+        private void HideCard(int i)
+        {
+            PictureBox card = GetCard(i);
+            card.Visible = false;
+        }
+
+        //-MWB 1/16/2019
+        private void HideAllCards()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                HideCard(i);
+            }
+        }
+
+        // shows a picture box
+        //-MWB 1/16/2019
+        private void ShowCard(int i)
+        {
+            PictureBox card = GetCard(i);
+            card.Image = Image.FromFile(System.Environment.CurrentDirectory + "\\Cards\\" + GetCardFilename(i) + ".jpg");
+        }
+
+        //a small method I wrote inorder to check if the cards were sufficiently randomized.-MWB 1/16/2019
+        private void ShowAllCards()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                ShowCard(i);
+            }
+        }
+
+        // disables a picture box
+        private void DisableCard(int i)
+        {
+            PictureBox card = GetCard(i);
+            card.Enabled = false;
+        }
+
+        private void DisableAllCards()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                DisableCard(i);
+            }
+        }
+
+        private void EnableCard(int i)
+        {
+            PictureBox card = GetCard(i);
+            card.Enabled = true;
+        }
+
+        private void EnableAllCards()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                EnableCard(i);
+            }
+        }
+    
+        private void EnableAllVisibleCards()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                PictureBox card = GetCard(i);
+                if (card.Visible == true)
+                {
+                    card.Enabled = true;
+                }
+
+            }
+        }
+
+        #endregion
+
+        #region EventHandlers
+        private void boardForm_Load(object sender, EventArgs e)
+        {
+            /* 
+             * Fill the picture boxes with filenames
+             * Shuffle the cards
+             * Load all of the card backs - 
+             *      While you're testing you might want to load all of card faces
+             *      to make sure that the cards are loaded successfully and that
+             *      they're shuffled.  If you get all 2s, something is wrong.
+             * 
+            */
+            
+            FillCardFilenames();
+            ShuffleCards();
+            //LoadAllCardFronts();      //A call to a small method I wrote inorder to check if the cards were sufficiently randomized.  -MWB 1/18/2019
+            LoadAllCardBacks();
+            
+
+        }
+
+        private void card_Click(object sender, EventArgs e)
+        {
+            PictureBox card = (PictureBox)sender;
+            int cardNumber = int.Parse(card.Name.Substring(4));
+            
+
+            /* 
+             * if the first card isn't picked yet
+             *      save the first card index
+             *      load the card
+             *      disable the card
+             *  else (the user just picked the second card)
+             *      save the second card index
+             *      load the card
+             *      disable all of the cards
+             *      start the flip timer
+             *  end if
+            */
+            if (firstCardNumber == NOT_PICKED_YET)
+            {
+                firstCardNumber = cardNumber;
+                LoadCard(cardNumber);
+                DisableCard(cardNumber);
+                
+            }
+            else
+            {
+                
+                secondCardNumber = cardNumber;
+                LoadCard(cardNumber);
+                DisableAllCards();
+                //MessageBox.Show("card click else statement");
+                //flipTimer = new System.Threading.Timer(1000);
+                //flipTimer_Tick(sender, e
+                flipTimer.Enabled = true;
+            }
+        }
+
+        private void flipTimer_Tick(object sender, EventArgs e)
+        {
+            /*
+             * stop the flip timer
+             * if the first card and second card are a match
+             *      increment the number of matches
+             *      hide the first card
+             *      hide the second card
+             *      reset the first card number
+             *      reset the second card number
+             *      if the number of matches is 10
+             *          show a message box
+             *      else
+             *          enable all of the cards left on the board
+             *      end if
+             * else
+             *      flip the first card back over
+             *      flip the second card back over
+             *      reset the first card number
+             *      reset the second card number
+             *      enable all of the cards left on the board
+             * end if
+             * The flip timer is an object on the boardForm.cs-MWB 1/18/2019
+             */
+            flipTimer.Enabled = false;
+            if (IsMatch(firstCardNumber, secondCardNumber) == true)
+            {
+                matches += 1;
+                HideCard(firstCardNumber);
+                HideCard(secondCardNumber);
+                firstCardNumber = NOT_PICKED_YET;
+                secondCardNumber = NOT_PICKED_YET;
+                if (matches == 10)
+                {
+                    MessageBox.Show("number of matches is 10");
+                }
+                else
+                {
+                    EnableAllVisibleCards();
+                }
+            }
+            else
+            {
+                LoadCardBack(firstCardNumber);
+                LoadCardBack(secondCardNumber);
+                firstCardNumber = NOT_PICKED_YET;
+                secondCardNumber = NOT_PICKED_YET;
+                EnableAllVisibleCards();
+            }
+        }
+        #endregion
+    }
+}
---
+++ tic tac toe/Program7_8/Form1.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program7_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ResetSquares();
+        }
+
+        const string USER_SYMBOL = "X";
+        const string COMPUTER_SYMBOL = "O";
+        const string EMPTY = "";
+        const int SIZE = 5;
+
+        // constants for the 2 diagonals
+        const int TOP_LEFT_TO_BOTTOM_RIGHT = 1;
+        const int TOP_RIGHT_TO_BOTTOM_LEFT = 2;
+
+        // constants for IsWinner
+        const int NONE = -1;
+        const int ROW = 1;
+        const int COLUMN = 2;
+        const int DIAGONAL = 3;
+
+        // This method takes a row and column as parameters and 
+        // returns a reference to a label on the form in that position
+        private Label GetSquare(int row, int column)
+        {
+            int labelNumber = row * SIZE + column + 1;
+            return (Label)(this.Controls["label" + labelNumber.ToString()]);
+        }
+
+        // This method does the "reverse" process of GetSquare
+        // It takes a label on the form as it's parameter and
+        // returns the row and column of that square as output parameters
+        private void GetRowAndColumn(Label l, out int row, out int column)
+        {
+            int position = int.Parse(l.Name.Substring(5));
+            row = (position - 1) / SIZE;
+            column = (position - 1) % SIZE;
+        }
+
+        // This method takes a row (in the range of 0 - 2) and returns true if 
+        // the row on the form contains 3 Xs or 3 Os.
+        // Use it as a model for writing IsColumnWinner
+
+        /*
+                private bool IsRowWinner(int row)
+                {
+                    if (GetSquare(row, 0).Text == GetSquare(row, 1).Text
+                            && GetSquare(row, 1).Text == GetSquare(row, 2).Text
+                            && GetSquare(row, 0).Text != EMPTY)
+                    {
+                        return true;
+                    }
+                    else
+                        return false;
+                }
+         */
+
+                 //* This is a better version of IsRowWinner.  Can you tell me what it does?
+                private bool IsRowWinner(int row)
+                {
+                    Label square = GetSquare(row, 0);
+                    string symbol = square.Text;
+                    for (int col = 1; col < SIZE; col++)
+                    {
+                        square = GetSquare(row, col);
+                        if (symbol == EMPTY || square.Text != symbol)
+                            return false;
+                    }
+                    return true;
+                }
+
+
+        // TODO:  Write these 3
+        /*private bool IsColumnWinner(int col)
+        {
+            if (GetSquare(0, col).Text == GetSquare(1, col).Text
+                    && GetSquare(1, col).Text == GetSquare(2, col).Text
+                    && GetSquare(0, col).Text != EMPTY)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        */
+
+        private bool IsColumnWinner(int col)
+        {
+            Label square = GetSquare(col, 0);
+            string symbol = square.Text;
+            for (int row = 1; row < SIZE; row++)
+            {
+                square = GetSquare(row, col);
+                if (symbol == EMPTY || square.Text != symbol)
+                    return false;
+            }
+            return true;
+        }
+        /*
+        private bool IsDiagonal1Winner()
+        {
+            if (GetSquare(0,0).Text == GetSquare(1,1).Text 
+                && GetSquare(1,1).Text == GetSquare(2,2).Text
+                && GetSquare(1,1).Text != EMPTY)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        */
+        private bool IsDiagonal1Winner()
+        {
+            int row = 0;
+            Label square = GetSquare(row, 0);
+            string symbol = square.Text;
+            for (row = 0; row < SIZE; row++)
+            {
+                square = GetSquare(row, row);
+                if (symbol == EMPTY || square.Text != symbol)
+                    return false;
+                
+            }
+            return true;
+        }
+
+        /*
+        private bool IsDiagonal2Winner()
+        {
+            if (GetSquare(0, 2).Text == GetSquare(1, 1).Text
+                && GetSquare(1, 1).Text == GetSquare(2, 0).Text
+                && GetSquare(1, 1).Text != EMPTY)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        */
+        private bool IsDiagonal2Winner()
+        {
+            int row = 0; 
+            int col = SIZE-1;
+            Label square = GetSquare(row, col);
+            string symbol = square.Text;
+            for (row = 0; row < SIZE; row++)
+            {
+                square = GetSquare(row, col);
+                if (symbol == EMPTY || square.Text != symbol)
+                    return false;
+                col--;
+            }
+            return true;
+        }
+
+
+        // This method determines if any row, column or diagonal on the board is a winner.
+        // It returns true or false and the output parameters will contain appropriate values
+        // when the method returns true.  See constant definitions at top of form.
+        private bool IsWinner(out int whichDimension, out int whichOne)
+        {
+            // rows
+            for (int row = 0; row < SIZE; row++)
+            {
+                if (IsRowWinner(row))
+                {
+                    whichDimension = ROW;
+                    whichOne = row;
+                    return true;
+                }
+            }
+            // columns
+            for (int column = 0; column < SIZE; column++)
+            {
+                if (IsColumnWinner(column))
+                {
+                    whichDimension = COLUMN;
+                    whichOne = column;
+                    return true;
+                }
+            }
+            // diagonals
+            if (IsDiagonal1Winner())
+            {
+                whichDimension = DIAGONAL;
+                whichOne = TOP_LEFT_TO_BOTTOM_RIGHT;
+                return true;
+            }
+            if (IsDiagonal2Winner())
+            {
+                whichDimension = DIAGONAL;
+                whichOne = TOP_RIGHT_TO_BOTTOM_LEFT;
+                return true;
+            }
+            whichDimension = NONE;
+            whichOne = NONE;
+            return false;
+        }
+
+        // I wrote this method to show you how to call IsWinner
+        private bool IsTie()
+        {
+            int winningDimension, winningValue;
+            return (IsFull() && !IsWinner(out winningDimension, out winningValue));
+        }
+
+        // TODO:  Write this one
+        private bool IsFull()
+        {
+            for (int row = 0; row < SIZE; row++)
+            {
+                for (int col = 0; col < SIZE; col++)
+                {
+                    Label square = GetSquare(row, col);
+                    if (square.Text == EMPTY)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        // Inside the click event handler you have a reference to the label that was clicked
+        // Use this method (and pass that label as a parameter) to disable just that one square
+        private void DisableSquare(Label square)
+        {
+            square.Click -= new System.EventHandler(this.label_Click);
+        }
+
+        // Setting the enabled property changes the look and feel of the cell.
+        // Instead, this code removes the event handler from each square.
+        // Use it when someone wins or the board is full to prevent clicking a square.
+        private void DisableAllSquares()
+        {
+            for (int row = 0; row < SIZE; row++)
+            {
+                for (int col = 0; col < SIZE; col++)
+                {
+                    Label square = GetSquare(row, col);
+                    DisableSquare(square);
+                }
+            }
+        }
+
+        // You'll need this method to allow the user to start a new game
+        private void EnableAllSquares()
+        {
+            for (int row = 0; row < SIZE; row++)
+            {
+                for (int col = 0; col < SIZE; col++)
+                {
+                    Label square = GetSquare(row, col);
+                    square.Click += new System.EventHandler(this.label_Click);
+                }
+            }
+        }
+
+        // This method "highlights" a column by making the font color for the cells in the column red
+        // It takes one parameter, the column and does not return a value
+        private void HighlightColumn(int col)
+        {
+            for (int row = 0; row < SIZE; row++)
+            {
+                Label square = GetSquare(row, col);
+                square.ForeColor = Color.Red;
+            }
+        }
+
+        // TODO:  Write these 2
+
+        private void HighlightRow(int row)
+        {
+            for (int col = 0; col < SIZE; col++)
+            {
+                Label square = GetSquare(row, col);
+                square.ForeColor = Color.Red;
+            }
+        }
+
+        private void HighlightDiagonal1()
+        {
+            for (int row = 0, col = 0; row < SIZE; row++, col++)
+            {
+                Label l = GetSquare(row, col);
+                l.ForeColor = Color.Red;
+            }
+        }
+
+        private void HighlightDiagonal2()
+        {
+            for (int row = 0, col = SIZE-1; row < SIZE; row++, col--)
+            {
+                Label l = GetSquare(row, col);
+                l.ForeColor = Color.Red;
+            }
+        }
+
+        // I needed this method to highlighting the diagonals work like rows and columns in the next method
+        private void HighlightDiagonal(int whichDiagonal)
+        {
+            if (whichDiagonal == TOP_LEFT_TO_BOTTOM_RIGHT)
+                HighlightDiagonal1();
+            else
+                HighlightDiagonal2();
+
+        }
+
+        // TODO:  finish this
+        private void HighlightWinner(string player, int winningDimension, int winningValue)
+        {
+            switch (winningDimension)
+            {
+                case ROW:
+                    HighlightRow(winningValue);
+                    resultLabel.Text = (player + " wins!");
+                    break;
+                case COLUMN:
+                    HighlightColumn(winningValue);
+                    resultLabel.Text = (player + " wins!");
+                    break;
+                case DIAGONAL:
+                    HighlightDiagonal(winningValue);
+                    resultLabel.Text = (player + " wins!");
+                    break;
+            }
+        }
+
+        // TODO:  Finish this
+        // The method should set all of the squares to empty and set the font color to black
+        private void ResetSquares()
+        {
+            DisableAllSquares();
+            EnableAllSquares();
+            for (int col = 0; col < SIZE; col++)
+            {
+                for (int row = 0; row < SIZE; row++)
+                {
+                    Label square = GetSquare(row, col);
+                    square.ForeColor = Color.Black;
+                    square.Text = EMPTY;
+                }
+            }
+            
+
+            resultLabel.Text = "";
+        }
+
+        // TODO:  Finish this
+        /*label square;
+         * do
+         * {
+         *      row=random number between 0 and 2
+         *      col=random number between 0 and 2
+         *      square = GetSquare(row, col);
+         * }
+         * while square is not empty
+         * put an O in the square
+         * disable the square
+         * 
+         * Notes: Randomness-look at rock paper scissors
+         * Random generator = new Random(); // before do
+         * int row = generator.Next(0, SIZE)
+         * 
+         * Notes: loop condition
+         * while(square.text!=EMPTY);
+         * label square = GetSquare(row, col);
+        */
+        private void MakeComputerMove()
+        {
+            Random numberGenerator = new Random();
+            Label square;
+            do
+            {
+                int col = numberGenerator.Next(0, SIZE);
+                int row = numberGenerator.Next(0, SIZE);
+                square = GetSquare(row, col);
+            }
+            while (square.Text != EMPTY);
+            square.Text = COMPUTER_SYMBOL;
+            DisableSquare(square);
+            /*
+            int winningDimension = NONE;
+            int winningValue = NONE;
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, 9);
+
+
+
+            label clickedLabel = randomNumber;
+            clickedLabel.Text = COMPUTER_SYMBOL;
+            DisableSquare(clickedLabel);
+            if (IsWinner(out winningDimension, out winningValue))
+            {
+                DisableAllSquares();
+                HighlightWinner("The computer", winningDimension, winningValue);
+            }
+            */
+        }
+        
+        /*
+         *put an "X" in the label
+         * disable that label
+         * If there is a winner then 
+         *      disable all squares
+         *      Highlight the winning row or column or diagonal and display a message
+         * else-if the board is not full
+         *      Computer makes a move
+         *      if there is a winner then
+         *          disable all the squares
+         *          highlight the winner
+         *      end if
+         * else
+         *      Display a message that says "It's a tie"
+         * end if
+        */
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            int winningDimension = NONE;
+            int winningValue = NONE;
+
+            Label clickedLabel = (Label)sender;
+            clickedLabel.Text = USER_SYMBOL;
+            DisableSquare(clickedLabel);
+            if (IsWinner(out winningDimension, out winningValue))
+            {
+                DisableAllSquares();
+                HighlightWinner("The User", winningDimension, winningValue);
+            }
+            else if (!IsFull())
+            {
+                MakeComputerMove();
+                if (IsWinner(out winningDimension, out winningValue))
+                {
+                    DisableAllSquares();
+                    HighlightWinner("The Computer", winningDimension, winningValue);
+                }
+            }
+            else
+                resultLabel.Text = "It's a tie";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResetSquares();
+            
+        }
+    }
+}
